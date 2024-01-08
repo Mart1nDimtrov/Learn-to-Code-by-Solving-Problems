d41677f1bd245c4d3a8c6045d1b1eb41ee2944e6
unknown <sly_racoon87@abv.bg>
2024-01-08 23:58:06 +0200

    Starting out with definite loops.

---
+++ 01. Gettings Started/dmopc15c7p2_not_a_wall_of_text_2.py
@@ -0,0 +1,5 @@
+# 6.DMOJ problem dmopc15c7p2, Not a Wall of Text
+
+line = input()
+total_words = line.count(' ') + 1
+print(total_words)
---
+++ noip18jp1_title_statistics.py
@@ -0,0 +1,4 @@
+# DMOJ problem noip18jp1, Title Statistics
+
+title = input().split(" ").count()
+
