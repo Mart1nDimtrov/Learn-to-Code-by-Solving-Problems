269524f45942afc8b78624a55fe00acadd5a3d3b
unknown <sly_racoon87@abv.bg>
2024-01-13 23:53:06 +0200

    DMOJ problem coci13c3p1, Rijeci

--- 03. Definite Loops/coci13c3p1_rijeci.py
+++ 03. Definite Loops/coci13c3p1_rijeci.py
@@ -1,1 +1,13 @@
-DMOJ problem coci13c3p1, Rijeci
+# 6.DMOJ problem coci13c3p1, Rijeci
+
+numbers_pressed = 8
+word = "A"
+#use another string to concatenate the new result
+new_word = []
+lenght = len(word)
+
+for number in range(numbers_pressed):
+	previous_number
+	n = number + 1
+
+print(f"{word.count('A')} {word.count('B')}")
