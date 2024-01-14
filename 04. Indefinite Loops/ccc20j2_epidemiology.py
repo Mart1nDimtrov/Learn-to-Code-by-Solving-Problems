a7268a031e00ba9671eb30693b05b6e448d4edd4
unknown <sly_racoon87@abv.bg>
2024-01-15 00:44:48 +0200

    DMOJ problem ccc20j2, Epidemiology

--- 04. Indefinite Loops/ccc20j2_epidemiology.py
+++ 04. Indefinite Loops/ccc20j2_epidemiology.py
@@ -1,1 +1,18 @@
 # 1.DMOJ problem ccc20j2, Epidemiology
+total_people = int(input())
+current_people = int(input())
+rate = int(input())
+days = 0
+current_rate = 0
+
+while total_people >= current_people:
+	if days == 0:
+		current_rate = rate * current_people 
+	else:
+		current_rate = current_rate * rate 
+	current_people = current_people + current_rate
+	days = days + 1
+
+print(days)
+
+
