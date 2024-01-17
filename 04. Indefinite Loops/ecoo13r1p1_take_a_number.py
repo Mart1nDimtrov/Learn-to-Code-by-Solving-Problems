6ec18e65898f37a1c875747e4d00bebeca09a531
unknown <sly_racoon87@abv.bg>
2024-01-17 22:51:17 +0200

    DMOJ problem ecoo13r1p1, Take a Number

--- 04. Indefinite Loops/ecoo13r1p1_take_a_number.py
+++ 04. Indefinite Loops/ecoo13r1p1_take_a_number.py
@@ -2,25 +2,26 @@
 
 number = int(input())
 command = input()
-total = 0
-waiting = 0
-served = 0
+total = number
+current_waiting = 0
+total_waiting = 0
 
 while command != "EOF":
 	if command == "TAKE":
-		waiting = waiting + 1
+		if total + 1 > 999:
+			total = 1
+		else:
+			total = total + 1
+		total_waiting = total_waiting + 1
+		current_waiting = current_waiting + 1
 	elif command == "SERVE":
-		served = served + 1
-		total = total + 1
-		waiting = waiting - 1
+		current_waiting = current_waiting - 1
 	elif command == "CLOSE":
-		total = total + waiting
-		print(waiting, served - waiting)
-		waiting = 0
-		served = 0
+		print(total_waiting, current_waiting, total)
+		current_waiting = 0
+		total_waiting = 0
 
 	command = input()
 
-print(total)
 
 
