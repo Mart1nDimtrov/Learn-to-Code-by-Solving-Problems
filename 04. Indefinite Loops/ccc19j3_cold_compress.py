2290dca299485b87718ff59f10a91bec510c0d3e
unknown <sly_racoon87@abv.bg>
2024-01-19 01:45:31 +0200

    DMOJ problem ccc19j3, Cold Compress

--- 04. Indefinite Loops/ccc19j3_cold_compress.py
+++ 04. Indefinite Loops/ccc19j3_cold_compress.py
@@ -6,20 +6,25 @@ for row in range(rows):
 	message = input()
 	message_len = len(message)
 	new_message = ""
+	symbol = ""
 	symbol_count = 0
 	for i in range(message_len):
 		if i == 0:
 			symbol = message[i]
-			symbol_count = symbol_count + 1
-		elif i == message_len - 1 and i == symbol:
-			symbol_count = symbol_count + 1
-			new_message = new_message + str(symbol_count) + " " + symbol
+			symbol_count = 1
+		elif i == message_len - 1:
+			if symbol == message[i]:
+				symbol_count = symbol_count + 1
+				new_message = new_message + str(symbol_count) + " " + symbol
+			else:
+				new_message = new_message + str(symbol_count) + " " + symbol + " "
+				new_message = new_message + str(1) + " " + message[i]
 		elif symbol == message[i]:
 			symbol_count = symbol_count + 1
-		elif i != symbol:
+		elif symbol != message[i]:
 			new_message = new_message + str(symbol_count) + " " + symbol + " "
+			symbol = message[i]
 			symbol_count = 1
-			symbol = new_message[i]
 
 	print(new_message)
 
