539ed6c4b8abad3b2737ba1169a795d314f8abf1
unknown <sly_racoon87@abv.bg>
2024-02-04 15:07:46 +0200

    DMOJ problem bpc1j1, Cantor Table

--- 02. Making Decisions/bpc1j1_cantor_table.py
+++ 02. Making Decisions/bpc1j1_cantor_table.py
@@ -1,5 +1,25 @@
 # DMOJ problem bpc1j1, Cantor Table
 
 n_term = int(input())
+count = 0
+if n_term == 1:
+	print("1/1")
+elif n_term == 2:
+	print("1/2")
+else:
+	for i in range(1,n_term):
+		temp_count = count
 
-for i in range()
+		for j in range(1,i+1):
+			if i % 2 == 0:
+				temp_count = count + j
+			elif i % 2 == 1:
+				temp_count = count + ((i - j) + 1)
+
+			if temp_count == n_term:
+				print(f"{j}/{i-(j-1)}")
+				break
+
+		count = count + i
+		if temp_count == n_term:
+				break
