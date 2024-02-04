4cd4cbfcb7200d36ad6d241bab0760ffc3a053f8
unknown <sly_racoon87@abv.bg>
2024-02-04 16:31:56 +0200

    DMOJ problem sac22cc3jp2, Normal Lines

---
+++ 02. Making Decisions/sac22cc3jp2_normal_lines.py
@@ -0,0 +1,12 @@
+# DMOJ problem sac22cc3jp2, Normal Lines
+
+line_1 = input().split()
+line_2 = input().split()
+
+line_1_x = int(line_1[0])
+line_1_y = int(line_1[1])
+line_2_x = int(line_2[0])
+line_2_y = int(line_2[1])
+
+if line_1_x == line_2_x or line_2_y == line_1_y:
+
