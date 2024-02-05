9488f77e14fd80252f8c289ebcaed2e578b2d4dd
unknown <sly_racoon87@abv.bg>
2024-02-05 23:30:46 +0200

    DMOJ problem sac22cc5jp1, Monkey, Type!

---
+++ 01. Gettings Started/sac22cc5jp1_monkey_type.py
@@ -0,0 +1,8 @@
+# DMOJ problem sac22cc5jp1, Monkey, Type!
+
+number_of_words = int(input())
+number_of_seconds = int(input())
+words_per_minute = int(input())
+
+print(words_per_minute - number_of_words)
+
