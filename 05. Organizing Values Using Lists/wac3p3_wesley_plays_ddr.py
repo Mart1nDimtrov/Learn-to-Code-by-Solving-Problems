b8bbea8070dfc6466efaac8f3a143901f57f43a4
unknown <sly_racoon87@abv.bg>
2024-02-08 19:34:32 +0200

    DMOJ problem wac3p3, Wesley Plays DDR

--- 05. Organizing Values Using Lists/wac3p3_wesley_plays_ddr.py
+++ 05. Organizing Values Using Lists/wac3p3_wesley_plays_ddr.py
@@ -4,46 +4,30 @@ moves = input()
 n_combos = int(input())
 l_moves = len(moves)
 combos = []
-moves_copy = [moves]
+
 for i in range(n_combos):
 	combo_input = input().split()
 	move = combo_input[0]
 	points = int(combo_input[1])
 	combos.append([move, points])
 
-
+combos.sort(key=lambda x: len(x[0]), reverse=True)
+l_combos = len(combos)
 total = 0
 i = 0
-index = 0
-end = 0
-previous_longest = 0
 
-for i_2 in range(len(moves)):
-	if combos[i_2][0] == moves[0:len(combos[i_2])+1]:
-		if previous_longest < len(combos[i_2][0]):
-			previous_longest = len(combos[i_2][0])
-			i = i_2
-
-if previous_longest == 0:
-	i = 1
-
-
-while i < len(combos):
-
-	if len(combos[i][0]) > len(moves):
-		break
-
-	if combos[i][0] in moves:
-		total = total + combos[i][1]
-		index = moves.index(combos[i][0])
-		end = index + len(combos[i][0])
-		if end > len(moves):
+while i < len(moves):
+	has_match = 0
+	for j in range(l_combos):
+		l_combo = len(combos[j][0])
+		end = i + l_combo
+		if combos[j][0] == moves[i:end]:
+			has_match = 1
+			total = total + combos[j][1]
+			i = i + l_combo
 			break
-		moves = moves[end:]
-		i = i
-	else:
+
+	if has_match == 0:
 		i = i + 1
 
-#if total + l_moves == 86592:
-#	print(i_2, previous_longest, combos[i-2], combos[i-3])
 print(total + l_moves)
