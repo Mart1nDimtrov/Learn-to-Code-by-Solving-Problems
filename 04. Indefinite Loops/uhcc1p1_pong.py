9bacc1b1e725791eabd061354b27dd342ed8d281
unknown <sly_racoon87@abv.bg>
2024-02-20 00:12:14 +0200

    DMOJ problem uhcc1p1, PONG!

--- 04. Indefinite Loops/uhcc1p1_pong.py
+++ 04. Indefinite Loops/uhcc1p1_pong.py
@@ -7,32 +7,31 @@ z = int(players_input[2])
 l = int(input())
 player1_points = 0
 player2_points = 0
-is_won = 0
+won = 0
 
 if x >= y:
 	player2_points = player2_points + y
+	if player2_points >= l and won == 0:
+		won = 2
 	player1_points = player1_points + z
-	if player2_points > l:
-		is_won = 1
-		print(2)
-	if player1_points > l:
-		is_won = 1
-		print(1)
+	if player1_points >= l and won == 0:
+		won = 1
 elif y > x:
 	player1_points = player1_points + x
-	if player1_points == l:
-		is_won = 1
-		print(1)
+	if player1_points >= l and won == 0:
+		won = 1
 
 while player1_points < l and player2_points < l:
 	if player1_points < player2_points:
 		player1_points = player1_points + x
+		if player1_points >= l and won == 0:
+			won = 1
 	elif player2_points <= player1_points:
-		player2_points = player2_points + y 
+		player2_points = player2_points + y
+		if player2_points >= l and won == 0:
+			won = 2
 		player1_points = player1_points + z
+		if player1_points >= l and won == 0:
+			won = 1
 
-if is_won == 0:
-	if player1_points > player2_points:
-		print(1)
-	elif player1_points < player2_points:
-		print(2)
+print(won)
