fb2c45d67d8f0a3a28df5cf6eed2a6967fe47395
unknown <sly_racoon87@abv.bg>
2024-02-24 13:26:30 +0200

    11.DMOJ problem coci20c2p1, Crtanje

--- 05. Organizing Values Using Lists/coci20c2p1_crtanje.py
+++ 05. Organizing Values Using Lists/coci20c2p1_crtanje.py
@@ -13,20 +13,18 @@ for i in range(days):
 		net_worth_sum = net_worth_sum + 1
 	elif net_worth[i] == "-":
 		net_worth_sum = net_worth_sum - 1
-		net_worth_changes.append([-1, net_worth_sum, i])
+		net_worth_changes.append(["\\", net_worth_sum, i])
 	elif net_worth[i] == '=':
-		net_worth_changes.append([0, net_worth_sum, i])
+		net_worth_changes.append(["_", net_worth_sum, i])
 
 min_row = min(x[1] for x in net_worth_changes)
 max_row = max(x[1] for x in net_worth_changes)
-print(net_worth_changes)
-print(min_row)
-print(max_row)
 
 for r in range(max_row, min_row-1, -1):
 	for d in range(days):
 		if ([x for x in net_worth_changes if x[1] == r and x[2] == d]):
-			print(f"+", end="")
+			element = [x[0] for x in net_worth_changes if x[1] == r and x[2] == d][0]
+			print(f"{element}", end="")
 		else:
 			print(f".", end="")
 	print("\n")
