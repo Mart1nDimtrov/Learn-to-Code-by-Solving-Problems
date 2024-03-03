2b1bb71bf83dd71aa86a13d2654c5f8f22a239da
unknown <sly_racoon87@abv.bg>
2024-03-03 12:17:19 +0200

    DMOJ problem mccc6j3, RGB Words

--- 05. Organizing Values Using Lists/mccc6j3_rgb_words.py
+++ 05. Organizing Values Using Lists/mccc6j3_rgb_words.py
@@ -1,29 +1,27 @@
 # DMOJ problem mccc6j3, RGB Words
 
-length = int(input())
+word_length = int(input())
 word = input()
+count = 0
 
-rgb_count = 0
-i = 0
-beginning = -1
-while i < len(word):
-	r_count = 0
-	g_count = 0
-	b_count = 0
-
-	for j in range(i, len(word)):
-		#print(i,j)
+for i in range(word_length):
+	r = 0
+	g = 0
+	b = 0
+	for j in range(i, word_length):
 		if word[j] == "R":
-			r_count = r_count + 1
-			i = j
-		elif word[j] == "G" and r_count == 1 and g_count == 0:
-			g_count = 1
-		elif word[j] == "G" and r_count == 1 and g_count == 1:
+			r = 1
+		elif word[j] == "G" and r == 0:
 			break
-		elif word[j] == "B" and r_count == 1 and g_count == 1:
-			b_count = b_count + 1
-			
-	rgb_count = rgb_count + b_count
-	i = i + 1
+		elif word[j] == "B" and g == 0 and r == 0:
+			break
+		elif word[j] == "G" and g == 0 and r == 1:
+			g = 1
+		elif word[j] == "G" and g == 1:
+			g = 1
+			break
+		elif word[j] == "B" and g == 1:
+			b = 1
+			count = count + 1
 
-print(rgb_count)
+print(count)
