98c46f3f3bf995cf0997ba4fa15092f4d6e35a4b
unknown <sly_racoon87@abv.bg>
2024-03-07 01:18:44 +0200

    DMOJ problem ccc99s1, Card Game

--- 05. Organizing Values Using Lists/ccc99s1_card_game.py
+++ 05. Organizing Values Using Lists/ccc99s1_card_game.py
@@ -5,13 +5,13 @@ def check_result(position, card, deck):
 	cards_length = 52
 	high_cards_arr = ["ace", "king", "queen", "jack"]
 
-	if card == "ace" and (cards_length - 5) >= position and not (x for x in deck[position+1:position+5] if x in high_cards_arr):
+	if card == "ace" and (cards_length - 5) >= position and len([x for x in deck[position+1:position+5] if x in high_cards_arr]) == 0:
 		result = 4
-	elif card == "king" and (cards_length - 4) >= position and not (x for x in deck[position+1:position+4] if x in high_cards_arr):
+	elif card == "king" and (cards_length - 4) >= position and len([x for x in deck[position+1:position+4] if x in high_cards_arr]) == 0:
 		result = 3
-	elif card == "queen" and (cards_length - 3) >= position and not (x for x in deck[position+1:position+3] if x in high_cards_arr):
+	elif card == "queen" and (cards_length - 3) >= position and len([x for x in deck[position+1:position+3] if x in high_cards_arr]) == 0:
 		result = 2
-	elif card == "jack" and (cards_length - 2) >= position and not (x for x in deck[position+1:position+2] if x in high_cards_arr):
+	elif card == "jack" and (cards_length - 2) >= position and len([x for x in deck[position+1:position+2] if x in high_cards_arr]) == 0:
 		result = 1
 
 	return result
@@ -24,13 +24,19 @@ for i in range(52):
 	card = deck.append(input())
 
 for i_2 in range(52):
+	points = 0
 	if i_2 % 2 == 0:
-		player_a_points = player_a_points + check_result(position=i_2, card=deck[i_2], deck=deck)
+		points = check_result(position=i_2, card=deck[i_2], deck=deck)
+		if points > 0:
+			print(f"Player A scores {points} point(s).")
+		player_a_points = player_a_points + points
 	elif i_2 % 2 == 1:
-		player_b_points = player_b_points + check_result(position=i_2, card=deck[i_2], deck=deck)
-	print(check_result(position=i_2, card=deck[i_2], deck=deck))
+		points = check_result(position=i_2, card=deck[i_2], deck=deck)
+		if points > 0:
+			print(f"Player B scores {points} point(s).")
+		player_b_points = player_b_points + points
 
-print(player_a_points)
-print(player_b_points)
+print(f"Player A: {player_a_points} point(s).")
+print(f"Player B: {player_b_points} point(s).")
 	
 
