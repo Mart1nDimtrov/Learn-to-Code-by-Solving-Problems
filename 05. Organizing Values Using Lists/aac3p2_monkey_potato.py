2006768920213e5cc4d1c6bc35afd94737d17163
unknown <sly_racoon87@abv.bg>
2024-03-20 01:05:45 +0200

    DMOJ problem aac3p2, Monkey Potato

--- 05. Organizing Values Using Lists/aac3p2_monkey_potato.py
+++ 05. Organizing Values Using Lists/aac3p2_monkey_potato.py
@@ -1,13 +1,17 @@
 # DMOJ problem aac3p2, Monkey Potato
 
-digit_input = list(map(int, input().split()))
-k = digit_input[0]
-d = digit_input[1]
-digits = list(map(int, input().split()))
+digit_input = input().split()
+k = int(digit_input[0])
+d = int(digit_input[1])
+digits = input().split()
 
-final_digit = str(min([d for d in digits if d > 0])) * k
+min_digit = ""
+min_digit = str(min([int(d) for d in digits if d != "0"], default="0"))
 
-if final_digit[0] != "0":
-	print(final_digit)
-else:
+if min_digit == "0":
 	print(-1)
+elif "0" in digits and k > 2:
+	print(min_digit + ("0" * (k - 2)) + min_digit)
+else:
+	print(min_digit * k)
+
