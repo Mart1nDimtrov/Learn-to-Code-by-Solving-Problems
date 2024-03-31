9426255ffa4075554250895244f615b601ae0263
unknown <sly_racoon87@abv.bg>
2024-03-31 11:01:37 +0300

    DMOJ problem ccc12j3, Icon Scaling

---
+++ 03. Definite Loops/ccc12j3_icon_scaling.py
@@ -0,0 +1,12 @@
+# DMOJ problem ccc12j3, Icon Scaling
+
+k = int(input())
+
+for i in range(k):
+	print(("*" * k) + ("x" * k) + ("*" * k))
+
+for i in range(k):
+	print((" " * k) + ("x" * k * 2))
+
+for i in range(k):
+	print(("*" * k) + (" " * k) +("*" * k))
