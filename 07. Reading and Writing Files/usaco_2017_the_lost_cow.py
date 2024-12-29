12cc9ab184c94d21dfbda35c0f2bbf5e2008705b
unknown <sly_racoon87@abv.bg>
2024-12-29 10:53:09 +0200

    # USACO 2019 December Bronze Contest problem Cow Gymnastics

---
+++ 07. Reading and Writing Files/usaco_2017_the_lost_cow.py
@@ -0,0 +1,38 @@
+# USACO 2017 US Open Bronze Contest problem The Lost Cow
+
+input_file = open('lostcow.in', 'r')
+output_file = open('lostcow.out', 'w')
+
+line = input_file.readline().split()
+position_farmer = int(line[0])
+position_cow = int(line[1])
+distance = 1
+distance_sum = 0
+previous_distance = 0
+up = 1
+
+while True:
+
+	distance_sum = distance_sum + (distance + previous_distance)
+
+	if up == 1 and position_cow > position_farmer and position_farmer + distance >= position_cow:
+		distance_sum = distance_sum - ((position_farmer + distance) - position_cow)
+		break
+	elif up == 0 and position_cow < position_farmer and position_farmer - distance <= position_cow:
+		distance_sum = distance_sum - (position_cow - (position_farmer - distance))
+		break
+
+	if up == 1:
+		up = 0
+
+	else:
+		up = 1
+
+	previous_distance = distance
+	distance = distance * 2
+
+print(distance_sum)
+
+
+
+
---
+++ 07. Reading and Writing Files/usaco_2019_cow_gymnastics.py
@@ -0,0 +1,25 @@
+# USACO 2019 December Bronze Contest problem Cow Gymnastics
+
+input_file = open('gymnastics.in', 'r')
+output_file = open('gymnastics.out', 'w')
+
+line = input_file.readline().split()
+practice_sessions = int(line[0])
+n_cows = int(line[1])
+cow_results = {}
+
+for n_session in range(0, practice_sessions):
+	line = list(map(int, input_file.readline().split()))
+
+	for cow_n in range(0, n_cows):
+		if n_session == 0:
+			cow_results[line[cow_n]] = [0 for x in range(0, n_cows)]
+		for cow_n_2 in range(0, n_cows):
+			if cow_n < cow_n_2:
+				if n_session == 0:
+					cow_results[line[cow_n]][line[cow_n_2]-1] = 1
+			elif cow_n > cow_n_2:
+				cow_results[line[cow_n]][line[cow_n_2]-1] = 0
+					
+print(sum([sum(value) for key, value in cow_results.items()]))
+		
